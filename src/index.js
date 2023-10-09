6fd36f26a38a0473b2417ec32e7d91273a758f7d
Vitali Kuntsevich <kvadesstudio@gmail.com>
2023-10-09 17:22:59 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let symbols = [];
+    let morseSymb = [];
+    let outputLineup = [];
+
+    //an array of encoded letters, the length of each encoded letter is 10
+    for (let i = 0; i < expr.length; i = i + 10) {
+        symbols.push(expr.substr(i, 10));
+    }
+
+    symbols.forEach((elem) => {
+        if (elem == "**********") outputLineup.push(" ");
+        let part = elem.split("");
+
+        //Remove zeros
+        for (let i = 0; i < part.length; i++) {
+            if (part[i] == 0) {
+                part.shift();
+                i = -1;
+            } else break;
+        }
+        //10 is a dot, 11 is a dash
+        for (let i = 0; i < part.length; i = i + 2) {
+            if (part[i].concat(part[i + 1]) == "10") {
+                morseSymb.push(".");
+            } else if (part[i].concat(part[i + 1]) == "11") {
+                morseSymb.push("-");
+            }
+        }
+        let morseLine = morseSymb.join("");
+
+        for (let key in MORSE_TABLE) {
+            if (key == morseLine) {
+                outputLineup.push(MORSE_TABLE[key]);
+            }
+        }
+        morseSymb = [];
+    });
+
+    let endOutput = outputLineup.join("");
+    return endOutput;
+
 }
 
 module.exports = {
-    decode
-}
+    decode,
+};
